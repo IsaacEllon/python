524c86ffe84ea104ca1fb0b584e17a0d2a0ea038
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2024-08-05 19:56:46 -0300

    1

---
+++ 2-sintaxe-basica/sintaxebasica.py
@@ -0,0 +1,16 @@
+# num1=1
+
+""""
+num2=2
+num3=3
+num4=4
+"""
+
+canal="CFB cursos"
+curso="curso de pyhon"
+print(canal + " - " + curso)
+
+if 10 > 4:
+    print("maior")
+    print("aula 2")
+print("Fim")
