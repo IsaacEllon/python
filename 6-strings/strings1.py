2dd80e75ffca4b2aa8ba5c85ea1a84fef8d92716
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2024-08-06 20:51:02 -0300

    1

--- 6-strings/strings1.py
+++ 6-strings/strings1.py
@@ -1,4 +1,12 @@
-curso="Curso de python"
+curso="Curso de python "
 
-print(curso[0:5])
-print("Tamanho: " + len(curso)) 
+
+
+#print(curso[0:5])
+#print(curso.strip()) #strip - retira os espaços
+#print(curso.lower().strip()) #lower - deixa faz a conversão para minusculo
+#print(curso.upper()) #upper - converte tudo em maiusculo 
+#print(curso.replace("python", "C#")) #replece - substitui uma string ou caracter por outra string ou caracter.
+a=curso.split(" ") # dentro do parêntese fica o indicador de corte, nesse caso especifico é o espaço
+print(a[2]) #split - quando tiver um espaço ele vai fazer um corte
+print("Tamanho: " + str(len(curso))) #len - informa o tamanho
---
+++ 6-strings/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+a=curso.split(" ")
+print(a[0]) #split - quando tiver um espaço ele vai fazer um corte
