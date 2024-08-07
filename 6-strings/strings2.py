ed4bfc5dbc6d42fdb01f6ad8232d8abe8445f1ba
Isaac Ellon <isaacellon@gmail.com>
2024-08-07 16:58:05 -0300

    1

---
+++ 6-strings/strings2.py
@@ -0,0 +1,25 @@
+"""
+texto="Curso de python "
+palavra="python"
+
+res=palavra.upper() in texto.upper() #in - verifica se tem a palavra python na var curso
+print(res)
+"""
+"""
+curso="Curso de Python"
+canal="CFB Cursos"
+
+res=curso+" do canal " +canal
+
+print(res)
+""" 
+cidade="Eusébio"
+dia=7
+mes="Agosto"
+ano=2024
+canal="CFB Cursos"
+
+#data=cidade+ ", "+str(dia)+" de "+mes+" de "+str(ano)
+data="{}, {} de {} de {}\n \"{}\""
+print(data.format(cidade,dia,mes,ano,canal))
+
---
+++ 7-tipo-boolean/tipo-boolean.py
@@ -0,0 +1,3 @@
+aula=""
+
+print(bool(aula))
