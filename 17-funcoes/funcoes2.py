3101b14471111d44c2839775c38a96a01d129809
Isaac Ellon <isaacellon@gmail.com>
2024-08-21 14:53:10 -0300

    1

---
+++ 17-funcoes/funcoes2.py
@@ -0,0 +1,9 @@
+n1=15
+n2=7
+
+def somar():
+    r=n1+n2
+    print("Soma de " + str(n1) + " e " + str(n2) + " = " + str(r))
+    print("Youtube.com\n")
+
+somar()
--- 17-funcoes/funcoess1.py
+++ 17-funcoes/funcoess1.py
@@ -1,4 +1,30 @@
+n1=10
+n2=5
+
 def somar():
-    print("Curso de python - CFB Cursos")
-    print("Youtube.com")
-somar()
+    r=n1+n2
+    print("Soma de " + str(n1) + " e " + str(n2) + " = " + str(r))
+    print("Youtube.com\n")
+
+def subtrair():
+    r=n1-n2
+    print("Subtração de "+ str(n1)+ " e " + str(n2) + " = " +str(r))
+    print("Youtube.com\n")
+
+def multiplicar():
+    r=n1*n2
+    print("multiplicação de "+ str(n1)+ " e " + str(n2) + " = " +str(r))
+    print("Youtube.com\n")
+
+def dividir():
+    r=n1/n2
+    print("Divisão de "+ str(n1)+ " e " + str(n2) + " = " + str(r))
+    print("Youtube.com\n")
+
+def calculos():
+    somar()
+    subtrair()
+    multiplicar()
+    dividir()
+
+calculos()
