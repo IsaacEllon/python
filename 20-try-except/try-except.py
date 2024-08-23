1aaed9d52abca5ab50fe448a0a653deb31af07c5
Isaac Ellon <isaacellon@gmail.com>
2024-08-23 13:41:14 -0300

    1

---
+++ 20-try-except/try-except.py
@@ -0,0 +1,18 @@
+num="ISaac"
+
+if type(num) is int:
+    raise Exception("Somente números permitidos") #raise - gera uma execeção
+else:
+    print(num)
+"""
+x=10
+try:
+    print(x)
+
+except: # except - executa apenas se o try retornar erro
+    print("Erro no programa")
+else:
+    print("Tudo certo")
+finally: # finally - executa se o try retornar erro ou não
+    print("Fim do tratamento")
+"""
