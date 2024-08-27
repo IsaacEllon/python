e085a20b4acec3db9ab68ca8674775bd4da935c2
Isaac Ellon <isaacellon@gmail.com>
2024-08-27 15:27:36 -0300

    1

--- automação/automação.py
+++ automação/automação.py
@@ -1,3 +1,17 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-navegador = webdriver.Chrome()
+servico = Service(ChromeDriverManager().install())
+
+navegador = webdriver.Chrome(service=servico)
+# Passo 1:
+navegador.get("https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_videoselenium")
+
+# Passo 2:
+navegador.find_element('xpath', 
+                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[1]/div/input').send_keys("Lira")
+navegador.find_element('xpath', 
+                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[2]/div/input').send_keys("pythonimpressionador@gmail.com")
+# Passo 3:
+navegador.find_element('xpath', '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/button').click()
