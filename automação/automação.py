4d537c59edb03fbf0ab3227eed34cdb0ce4b1fce
Isaac Ellon <isaacellon@gmail.com>
2024-08-27 16:33:20 -0300

    1

--- automação/automação.py
+++ automação/automação.py
@@ -2,16 +2,13 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-servico = Service(ChromeDriverManager().install())
+servico = Service(ChromeDriverManager().install()) #identifica a versão do chrome e vai instalar o o chromedriver correspondente 
 
 navegador = webdriver.Chrome(service=servico)
-# Passo 1:
-navegador.get("https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_videoselenium")
 
-# Passo 2:
-navegador.find_element('xpath', 
-                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[1]/div/input').send_keys("Lira")
-navegador.find_element('xpath', 
-                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[2]/div/input').send_keys("pythonimpressionador@gmail.com")
-# Passo 3:
-navegador.find_element('xpath', '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/button').click()
+navegador.get("https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_videoselenium") #get - direciona para a página web
+navegador.find_element('xpath', '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[1]/div/input').send_keys("Isaac") #find_element - encontrar elemento na página web
+navegador.find_element('xpath','//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[2]/div/input').send_keys("isaacellon5@gmail.com") #send_keys - escreve no campo
+navegador.find_element('xpath','//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[3]/div/input').send_keys("85989878293")
+
+
