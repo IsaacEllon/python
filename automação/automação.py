23e79bbdb63d78b10e4100a0988156f3aeabbc48
Isaac Ellon <isaacellon@gmail.com>
2024-08-30 09:08:52 -0300

    1

--- automação/automação.py
+++ automação/automação.py
@@ -6,6 +6,7 @@ servico = Service(ChromeDriverManager().install()) #identifica a versão do chro
 
 navegador = webdriver.Chrome(service=servico)
 
+
 navegador.get("https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_videoselenium") #get - direciona para a página web
 navegador.find_element('xpath', '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[1]/div/input').send_keys("Isaac") #find_element - encontrar elemento na página web
 navegador.find_element('xpath','//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[2]/div/input').send_keys("isaacellon5@gmail.com") #send_keys - escreve no campo
